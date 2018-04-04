6456fa8418f06e62de50a19b15c08fa7da2c093e
201501054 <fredericamichell@gmail.com>
2018-04-04 23:28:31 +0700

    Tugas Modul04

---
+++ 2.js
@@ -0,0 +1,69 @@
+/* nomor 1 */
+/* var x = 20;
+var y = 10;
+
+var score x+=y;
+console.log(score); */
+
+/* nomor 2 */
+/* var x = 20;
+var y = 10;
+
+var score = x + y;
+console.log(score); */
+
+/* nomor 3 */
+/* var x = 20;
+var y = 10;
+
+var score x-=y;
+console.log(score); */
+
+/* nomor 4 */
+/* var x = 20;
+var y = 10;
+
+var score = x - y;
+console.log(score); */
+
+/* nomor 5 */
+/* var x = 20;
+var y = 10;
+
+var score x*=y;
+console.log(score); */
+
+/* nomor 6 */
+/* var x = 20;
+var y = 10;
+
+var score = x * y;
+console.log(score); */
+
+/* nomor 7 */
+/* var x = 20;
+var y = 10;
+
+var score x/=y;
+console.log(score); */
+
+/* nomor 8 */
+/* var x = 20;
+var y = 10;
+
+var score = x / y;
+console.log(score); */
+
+/* nomor 9 */
+/* var x = 20;
+var y = 10;
+
+var score x%=y;
+console.log(score); */
+
+/* nomor 10 */
+var x = 20;
+var y = 10;
+
+var score = x % y;
+console.log(score);
